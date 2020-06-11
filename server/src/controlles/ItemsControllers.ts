78a877da60a7b60c8f13c92c9e2b4e23aa823183
Raphael Martins <raphael@agecomnet.com.br>
2020-06-11 09:22:14 -0300

    Aula 03 part 1

--- server/src/controlles/ItemsControllers.ts
+++ server/src/controlles/ItemsControllers.ts
@@ -7,7 +7,7 @@ class ItemsController {
       return {
         id: item.id,
         name: item.title,
-        image_url: `http://localhost:3333/uploads/${item.image}`,
+        image_url: `http://192.168.0.103:3333/uploads/${item.image}`,
       };
     });
     return response.json(seralizedItems);
